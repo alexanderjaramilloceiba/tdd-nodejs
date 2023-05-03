7eaae4f99b1e1d082371cfdb1e3715d44ced58f3
Alexander Jaramillo <alexander.jaramillo@ceiba.com.co>
2023-05-03 02:46:02 -0500

    refactor

--- __tests__/sum.spec.ts
+++ __tests__/sum.spec.ts
@@ -1,7 +1,5 @@
+import { sum } from "../src/sum";
+
 test("adds 1 + 2 to equal 3", () => {
 	expect(sum(1, 2)).toBe(3);
 });
-
-function sum(num1: number, num2: number): number {
-	return 3;
-}
---
+++ src/sum.ts
@@ -0,0 +1,5 @@
+function sum(num1: number, num2: number): number {
+	return 3;
+}
+
+export { sum };
