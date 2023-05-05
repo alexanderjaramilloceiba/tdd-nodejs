7d37b4830802b45826463a51aa0da19fb5d8d78d
Alexander Jaramillo <alexander.jaramillo@ceiba.com.co>
2023-05-05 15:44:00 -0500

    agrupamos las pruebas de Claudiolator con describe

--- __tests__/claudiolator/add.spec.ts
+++ __tests__/claudiolator/add.spec.ts
@@ -1,28 +1,30 @@
 import { Claudiolator } from "../../src/modules/claudiolator/domain/model/claudiolator";
 
-let claudiolator: Claudiolator;
-beforeEach(() => {
-	// Jest will wait for this promise to resolve before running tests.
-	claudiolator = new Claudiolator();
-});
+describe(Claudiolator.name, () => {
+	let claudiolator: Claudiolator;
+	beforeEach(() => {
+		// Jest will wait for this promise to resolve before running tests.
+		claudiolator = new Claudiolator();
+	});
 
-test("sumar I y I", () => {
-	// Act
-	const result = claudiolator.add("I", "I");
-	// Assert
-	expect(result).toBe("II");
-});
+	test("sumar I y I", () => {
+		// Act
+		const result = claudiolator.add("I", "I");
+		// Assert
+		expect(result).toBe("II");
+	});
 
-test("sumar I y II", () => {
-	// Act
-	const result = claudiolator.add("I", "II");
-	// Assert
-	expect(result).toBe("III");
-});
+	test("sumar I y II", () => {
+		// Act
+		const result = claudiolator.add("I", "II");
+		// Assert
+		expect(result).toBe("III");
+	});
 
-test("sumar II y II", () => {
-	// Act
-	const result = claudiolator.add("II", "II");
-	// Assert
-	expect(result).toBe("IV");
+	test("sumar II y II", () => {
+		// Act
+		const result = claudiolator.add("II", "II");
+		// Assert
+		expect(result).toBe("IV");
+	});
 });
