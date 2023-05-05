fb5c5d19d3f493f4fae64ce92b316e478cb16d1a
Alexander Jaramillo <alexander.jaramillo@ceiba.com.co>
2023-05-05 13:13:12 -0500

    cambio de función a método de la clase Claudiolator

--- __tests__/claudiolator/add.spec.ts
+++ __tests__/claudiolator/add.spec.ts
@@ -1,6 +1,9 @@
 test("sumar I y I", () => {
-	expect(add("I", "I")).toBe("II");
+	expect(new Claudiolator().add("I", "I")).toBe("II");
 });
-function add(arg0: string, arg1: string): any {
-	return "II";
+
+class Claudiolator {
+	add(arg0: string, arg1: string): any {
+		return "II";
+	}
 }
