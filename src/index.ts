12aa7b7764e23eef2df20b31fca92794d00428f5
Alexander Jaramillo <alexander.jaramillo@ceiba.com.co>
2023-05-05 18:24:40 -0500

    a very ugly example of use of domain

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,37 @@
 import express from "express";
+import { Claudiolator } from "./modules/claudiolator/domain/model/claudiolator";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/post", async (req, res) => {
-	const { sample } = req.body;
-	res.json({ received: { sample } });
-});
+app.get("/:romanNumeral1/plus/:romanNumeral2", async (req, res) => {
+	const {
+		romanNumeral1,
+		romanNumeral2,
+	}: { romanNumeral1: string; romanNumeral2?: string } = req.params;
 
-app.get("/", async (req, res) => {
-	res.json({ ok: true });
-});
+	const decimalConverter = {
+		from: (romanNumeral: string): number =>
+			({
+				["I"]: 1,
+				["II"]: 2,
+			}[romanNumeral] || 0),
+		to: (decimal: number): string =>
+			({
+				[2]: "II",
+				[3]: "III",
+				[4]: "IV",
+			}[decimal] || ""),
+	};
 
-app.get("/param/:id", async (req, res) => {
-	const { id }: { id?: string } = req.params;
+	const claudiolator = new Claudiolator(decimalConverter);
 
-	res.json({ received: { id } });
+	res.json({ result: claudiolator.add(romanNumeral1, romanNumeral2) });
 });
 
 /* const server =  */ app.listen(3000, () =>
 	console.log(`
-🚀 Server ready at: http://localhost:3000
+🚀 Server ready at: http://localhost:3000/II/plus/I
 ⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`)
 );
